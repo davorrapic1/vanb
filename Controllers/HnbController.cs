e4a66e4d4dc0f4c8bd572bfe1f52188982218107
Davor Rapić <rapic.davor@gmail.com>
2022-06-25 15:27:21 +0200

    try catch, get xml

--- Controllers/HnbController.cs
+++ Controllers/HnbController.cs
@@ -3,28 +3,34 @@ using Microsoft.AspNetCore.Mvc;
 namespace vanb.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route ("[controller]")]
 public class HnbController : ControllerBase
 {
-    static readonly HttpClient client = new HttpClient();
-    
+    static readonly HttpClient client = new HttpClient ();
+
     private readonly ILogger<HnbController> _logger;
 
-    public HnbController(ILogger<HnbController> logger)
+    public HnbController (ILogger<HnbController> logger)
     {
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetHnbData")]
-    public async Task<ActionResult> GetHnbData()
+    [HttpGet (Name = "GetHnbData")]
+    public async Task<ActionResult> GetHnbData ()
     {
-       
-        HttpResponseMessage response = await client.GetAsync("https://api.hnb.hr/tecajn/v2?datum-primjene-od=2022-5-3&datum-primjene-do=2022-6-1&valuta=EUR&valuta=USD");
 
-        response.EnsureSuccessStatusCode();
-        string responseBody = await response.Content.ReadAsStringAsync();
+        try
+        {
+            HttpResponseMessage response = await client.GetAsync ("https://api.hnb.hr/tecajn/v2?datum-primjene-od=2022-5-3&datum-primjene-do=2022-6-1&valuta=EUR&valuta=USD&format=xml");
 
-        
-        return  Ok(responseBody);
+            response.EnsureSuccessStatusCode ();
+            string responseBody = await response.Content.ReadAsStringAsync ();
+
+            return Ok (responseBody);
+        }
+        catch (System.Exception e)
+        {
+            return StatusCode (500, e.Message);
+        }
     }
 }
